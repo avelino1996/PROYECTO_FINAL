6ffdb659d2cb22fa7721ba27e5afd74b7c44ba1d
Andrés <avelino-96@hotmail.com>
2022-08-26 11:59:57 +0200

    login completed

--- frontend/src/pages/CreateAccount.js
+++ frontend/src/pages/CreateAccount.js
@@ -1,32 +1,40 @@
 import React from 'react'
-import "./components/login.css"
+import "../components/login.css"
 
 export default function CreateAcoount() {
-  return (
+
+  const signUpForm = () => (
     <form className ='sign-box'>
-      <h2 className='control-text'>
-            Crear usuario
-        </h2>
-      <div className='control-form'>
-        <label className ='text-muted'>Name</label>
-        <input type ="text" 
-          className ='form-control'/>
-      </div>
-      <div className='control-form'>
-        <label className ='text-muted'>Email</label>
-        <input type ="email" 
-          className ='form-control'/>
-      </div>
-      <div className='control-form'>
-        <label className ='text-muted'>Password</label>
-        <input type ="password" 
-          className = 'form-control'/>
-      </div>
-      <div className='control-form'>
-        <button className ='btn btn-primary'>
-          crear
-        </button>
-      </div>
-    </form>
+    <h2 className='control-text'>
+          Crear usuario
+      </h2>
+    <div className='control-form'>
+      <label className ='text-muted'>Name</label>
+      <input type ="text" 
+        className ='form-control'/>
+    </div>
+    <div className='control-form'>
+      <label className ='text-muted'>Email</label>
+      <input type ="email" 
+        className ='form-control'/>
+    </div>
+    <div className='control-form'>
+      <label className ='text-muted'>Password</label>
+      <input type ="password" 
+        className = 'form-control'/>
+    </div>
+    <div className='control-form'>
+      <button className ='btn btn-primary'>
+        crear
+      </button>
+    </div>
+  </form>
+  
+  )
+  return (
+    <>
+    {signUpForm()}
+    </>
+   
   )
 }
--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,27 +1,118 @@
-import React from 'react'
-import "./components/login.css"
+import React, { useState } from 'react'
+import {  Navigate } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom';
+import { signin, authenticate, isAuthenticated } from './apiCore'
+import "../components/login.css"
 
-export default function CreateAcoount() {
-  return (
-    <form className ='sign-box'>
+
+export default function login() {
+  const navigate = useNavigate();
+
+    const [values, setValues] = useState({
+      email: '',
+      password: '',
+      error: '',
+      loading: false,
+      redirectToReferrer: false
+    });
+
+    const { email, password, loading, error, redirectToReferrer } = values;
+    const { user } = isAuthenticated();
+
+    const handleChange = name => event => {
+      setValues({ ...values, error: false, [name]: event.target.value })
+    }
+
+    const clickSubmit = (event) => {
+      event.preventDefault();
+      setValues({ ...values, error: false, loading: true })
+      signin({ email, password })
+        .then(data => {
+          if (data.error) {
+            setValues({ ...values, error: data.error, loading: false })
+          } else {
+            authenticate(
+              data, () => {
+                setValues({
+                  ...values,
+                  redirectToReferrer: true
+                })
+              }
+            )
+          }
+        })
+    }
+
+    const signInForm = () => (
+      <form className='sign-box'>
         <h2 className='control-text'>
-            Iniciar sesión
+          Iniciar sesión
         </h2>
-      <div className='control-form'>
-        <label className ='text-muted'>Email</label>
-        <input type ="email" 
-          className ='form-control'/>
+        <div className='control-form'>
+          <label className='text-muted'>Email</label>
+          <input 
+           onChange={handleChange('email')}
+           type="email"
+           className="form-control"
+           value={email}
+           />
+        </div>
+        <div className='control-form'>
+          <label className='text-muted'>Password</label>
+          <input 
+          onChange={handleChange('password')}
+          type="password"
+          className="form-control"
+          value={password}
+          />
+        </div>
+        <div className='control-form'>
+          <button onClick={clickSubmit} className='btn btn-primary'>
+            Entrar
+          </button>
+        </div>
+        <div>
+          registrarse
+          <button onClick={(e) => navigate('/CreateAccount')}>regrsitrarse</button>
+        </div>
+      </form>
+    )
+
+    const redirectUser = () => {
+      if (redirectToReferrer) {
+        if (user && user.role === 1) {
+          return <Navigate to="/history" />
+        } else {
+          return <Navigate to="/Contact" />
+        }
+      }
+      if (isAuthenticated()) {
+        return <Navigate to="/" />
+      }
+    }
+
+    const showError = () => (
+      <div className="alert alert-danget" style={{ display: error ? '' : 'none' }}>
+        {error}
       </div>
-      <div className='control-form'>
-        <label className ='text-muted'>Password</label>
-        <input type ="password" 
-          className = 'form-control'/>
-      </div>
-      <div className='control-form'>
-        <button className ='btn btn-primary'>
-          crear
-        </button>
-      </div>
-    </form>
-  )
-}
+    )
+
+    const showLoading = () => (
+      loading && (
+        <div className="alert alert-info">
+          <h2>Loading...</h2>
+        </div>
+      )
+    )
+
+    return (
+      <>
+      {showError()}
+      {showLoading()}
+      {redirectUser()}
+      {signInForm()}
+      </>
+
+    )
+  
+} 
